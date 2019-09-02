b3838160fec951e95ba6809c25811246bb7fb261
Vincent Honca <vincenth1985@gmail.com>
2019-09-02 11:31:00 +0200

    first commit with implementing method getitemsforticket

--- src/main/java/be/infernalwhale/dao/FoodItemDAO.java
+++ src/main/java/be/infernalwhale/dao/FoodItemDAO.java
@@ -1,11 +1,37 @@
 package be.infernalwhale.dao;
 
 import be.infernalwhale.model.FoodItem;
+import be.infernalwhale.model.Ticket;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class FoodItemDAO {
-    public List<FoodItem> getItemsForTicket(int ticketID) {
-        return null;
-    }
+
+
+    public List<FoodItem> getItemsForTicket(int ticketID) throws SQLException {
+
+        List<FoodItem> foodList = new ArrayList<>();
+
+        PreparedStatement statement = DBConnector
+                .getConnection()
+                .prepareStatement("SELECT * FROM foodItem WHERE ticket = ?");
+        statement.setInt(1, ticketID);
+
+        ResultSet rs = statement.executeQuery();
+
+
+        while (rs.next()) {
+
+            FoodItem foodItem = new FoodItem(new Ticket());
+
+            foodList.add(foodItem.setId(rs.getInt("ticket")));
+        }
+
+
+        return foodList;
+    } 
 }
